3bfba4434840795a0a965e9fa41a32b55370fc3c
trashboat182 <folieadeux182>
2016-03-30 16:04:54 -0400

    Login + CSS

--- src/app/components/login/login/login.controller.js
+++ src/app/components/login/login/login.controller.js
@@ -6,5 +6,14 @@ angular
 
 /** @ngInject */
 function LoginController($scope) {
+    $scope.login = {
+      email:'',
+      password:''
+    };
+
     console.log('Login Controller');
+    $scope.signin = function(){
+      console.log($scope.login.email);
+      console.log($scope.login.password);
+    };
 }
