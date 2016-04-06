822b04226c83232ed3ce5fdec868b95b4dc9e7df
trashboat182 <folieadeux182>
2016-04-06 17:02:13 -0400

    Adding upload Video, Musik, Image Options

--- src/app/components/dashboard/dashboar.controller.js
+++ src/app/components/dashboard/dashboar.controller.js
@@ -10,7 +10,7 @@ function DashboardController($scope,brand,ngDialog) {
     console.log('dashboard');
 
     $scope.showUploadDialog = function(){
-      $scope.loginErrorDialog = ngDialog.open({
+      $scope.dashboardOptionsDialog = ngDialog.open({
         template: 'app/components/dashboard/uploadFileDialog.html',
         className: 'ngdialog-theme-default ngdialog-transparent ngdialog-dashboard',
         closeByEscape : true,
@@ -18,6 +18,48 @@ function DashboardController($scope,brand,ngDialog) {
         showClose: true,
         scope: $scope
       });
+    };
+
+    $scope.showUploadImage = function(){
+      $scope.dashboardImageDialog = ngDialog.open({
+        template: 'app/components/dashboard/uploadImageDialog.html',
+        className: 'ngdialog-theme-default ngdialog-transparent ngdialog-dashboard',
+        closeByEscape : true,
+        closeByDocument: true,
+        showClose: true,
+        scope: $scope
+      });
+    };
+
+    $scope.showUploadVideo = function(){
+      $scope.dashboardVideoDialog = ngDialog.open({
+        template: 'app/components/dashboard/uploadVideoDialog.html',
+        className: 'ngdialog-theme-default ngdialog-transparent ngdialog-dashboard',
+        closeByEscape : true,
+        closeByDocument: true,
+        showClose: true,
+        scope: $scope
+      });
+    };
+
+    $scope.showUploadMusik = function(){
+      $scope.dashboardMusikDialog = ngDialog.open({
+        template: 'app/components/dashboard/uploadMusikDialog.html',
+        className: 'ngdialog-theme-default ngdialog-transparent ngdialog-dashboard',
+        closeByEscape : true,
+        closeByDocument: true,
+        showClose: true,
+        scope: $scope
+      });
+    };
+
+    $scope.closeUploadDialog = function(){
+      $scope.dashboardOptionsDialog.close();
+    };
+
+    $scope.uploadImages =function(){
+      $scope.closeUploadDialog();
+      $scope.showUploadImage();
     };
 
 }
