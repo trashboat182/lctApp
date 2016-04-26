de509c65297271e829d62642b734046092cbb263
Kevin <kevin.ruizvargas@gmail.com>
2016-04-26 11:39:50 -0600

    Adding file uppload service without file upload

--- src/app/components/dashboard/videos/uploadVideo.controller.js
+++ src/app/components/dashboard/videos/uploadVideo.controller.js
@@ -7,31 +7,64 @@ angular
 /** @ngInject */
 function UploadVideoController($scope,fileUploadService,API_ENDPOINT,Session,Rest) {
   console.log('upload video ctrl');
-  $scope.video = {};
+  $scope.video = {
+      title: '',
+      video: '',
+      emision: '',
+      campaign: '',
+      agency:'',
+      announcer: '',
+      creativeDirector:'',
+      artDirector:'',
+      designer: '',
+      production:'',
+      illustration: '',
+      redactor: '',
+      responsible: ''
+  };
   $scope.videoFile = {};
 
-  $scope.saveVideo = function(){
-    var file = $scope.videoFile;
-    var uploadUrl = API_ENDPOINT+'/upload';
+   $scope.emptyObjValueExists = function(){
+       return _.some($scope.video, function(value, key, obj){
+         return obj[key] === '';
+       });
+   };
 
-    fileUploadService.uploadFileToUrl(file,uploadUrl).then(function(response){
-      var user = Session.getUser();
-      var fileObj = $scope.video;
-      fileObj.videoFile = JSON.parse(response);
-      console.log('fileObj');
-      console.log(fileObj);
-      Rest.registerImage(user.username).customPUT(fileObj).then(function(){
-        console.log('file Saved');
-      })
-      .catch(function(e){
-        console.log('file NOT Saved');
-        console.log(e);
-      })
-    })
-    .catch(function(e){
-      console.log('uploaded failed');
-      console.log(e);
-    })
+   $scope.saveVideo = function(){
 
+//    var file = $scope.videoFile;
+//    var uploadUrl = API_ENDPOINT+'/upload';
+
+//    fileUploadService.uploadFileToUrl(file,uploadUrl).then(function(response){
+//      var user = Session.getUser();
+//      var fileObj = $scope.video;
+//      fileObj.videoFile = JSON.parse(response);
+//      console.log('fileObj');
+//      console.log(fileObj);
+//      Rest.registerImage(user.username).customPUT(fileObj).then(function(){
+//        console.log('file Saved');
+//      })
+//      .catch(function(e){
+//        console.log('file NOT Saved');
+//        console.log(e);
+//      })
+//    })
+//    .catch(function(e){
+//      console.log('uploaded failed');
+//      console.log(e);
+//    })
+
+       console.log($scope.emptyObjValueExists());
+       console.log(JSON.stringify($scope.video,null,4));
+      if(!$scope.emptyObjValueExists()){
+          var user = Session.getUser();
+          Rest.registerImage(user.username).customPUT($scope.video).then(function(){
+              console.log('file Saved');
+          })
+              .catch(function(e){
+                  console.log('file NOT Saved');
+                  console.log(e);
+              })
+      }
   };
 }
